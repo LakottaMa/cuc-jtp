a4bc1bce648472e6d63b0b03017290bdf7a9692b
Marcel Lakotta <marcel.lakotta@aol.de>
2024-05-24 20:14:13 +0200

    First structure & Firebase connection added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+    provideNoopAnimations(),
+    provideFirebaseApp(() => initializeApp({"projectId":"cuc-jtp","appId":"1:145776616225:web:f79279341454697d6d1460","storageBucket":"cuc-jtp.appspot.com","apiKey":"AIzaSyA4juqT5MIKSWiwT2DRP1hzpl4AkdcXiFM","authDomain":"cuc-jtp.firebaseapp.com","messagingSenderId":"145776616225"})), provideFirestore(() => getFirestore())]
 };
